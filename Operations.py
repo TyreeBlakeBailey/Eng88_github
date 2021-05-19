03a503ad2031c563b7c5ca6e9439202d5fef1ef3
Tyree <t9tyree@hotmail.co.uk>
2021-05-19 15:39:52 +0100

    tuples added

--- Operations.py
+++ Operations.py
@@ -1,5 +1,5 @@
-# Lists
-# Syntz ["London" ]
+Lists
+Syntz ["London" ]
 
 shopping_list = [ "juice", "strawberries", "yogurt", "chicken", "raspberry", "butter" ]
 #                   0           1              2        3           4           5
@@ -26,3 +26,13 @@ print(shopping_list.reverse())
 #prints the list one item at a time in a loop format
 for x in shopping_list:
     print(x)
+
+# Tuples
+
+essential = ("egg", "milk", "break")
+
+print(essential)
+print(type(essential))
+
+# replace bread with yogurt
+#essential(2) = "yogurt" #returns an error cannot change the value of a tuple
