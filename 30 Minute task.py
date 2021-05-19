2262537e6ded3e5a0cea1bf4c6c2f0a02249d7fe
Tyree <t9tyree@hotmail.co.uk>
2021-05-19 17:04:38 +0100

    attempted to check if the persons birtdhay has passed

--- 30 Minute task.py
+++ 30 Minute task.py
@@ -3,8 +3,12 @@ import datetime
 
 def Birth_Year(age):
     return int(datetime.datetime.now().strftime('%Y')) - int(age) - 1
+def Birthday_Pass(BirthDay): # function to check if the persons birthday has passed
+    BirthDay.strfttime('%d')
+
 
 name = input('What is your name?  ').capitalize()
 age = input("How old are you ?  ")
+Birth_Day = input("What is your birthday DD/MM")
 
 print("OMG {}, you are {} years old so you were born in {} ".format(name, age, Birth_Year(age)))
