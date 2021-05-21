cc17712e8a258080fb7a54a87f69a77688e79d40
Tyree <t9tyree@hotmail.co.uk>
2021-05-21 09:46:04 +0100

    Started FizzBuzz. Prints Number 0 to 100

---
+++ FizzBuzz.py
@@ -0,0 +1,11 @@
+#Write a program that prints the numbers from 1 to 100.
+#But for multiples of three print “Fizz” instead of the number
+#For the multiples of five print “Buzz”.
+#For numbers which are multiples of both three and five print “FizzBuzz”."'''
+
+x = 0
+while x <= 100:
+    print(x)
+    x += 1
+
+
